6dc40cbfad2c3aa4ac7248b66cf622565ee6673d
Andrea Peltrin <andrea@passiomatic.com>
2022-03-25 17:44:11 +0100

    Update level

--- src/Levels.elm
+++ src/Levels.elm
@@ -5,6 +5,6 @@ import AltMath.Vector2 exposing (Vec2, vec2)
 import Entity exposing (EntityType(..), PlayerData)
 
 level1 = {
-     spawns = [ { position = vec2 120 40, side = 1, type_ = Player (PlayerData 0)}, { position = vec2 704 120, side = 1, type_ = Gem}, { position = vec2 268 68, side = 1, type_ = Gem}, { position = vec2 520 120, side = 1, type_ = Gem} ]
-     , walls = [ { p1 = vec2 4 -4, p2 = vec2 232 -4, normal = vec2 0 1}, { p1 = vec2 232 -4, p2 = vec2 232 36, normal = vec2 -1 0}, { p1 = vec2 232 36, p2 = vec2 388 36, normal = vec2 0 1}, { p1 = vec2 388 36, p2 = vec2 388 80, normal = vec2 -1 0} ]
+     spawns = [ { position = vec2 576 64, side = 1, type_ = Gem}, { position = vec2 208 112, side = 1, type_ = Gem}, { position = vec2 400 112, side = 1, type_ = Gem}, { position = vec2 304 48, side = 1, type_ = Player (PlayerData 0)}, { position = vec2 48 64, side = 1, type_ = Gem} ]
+     , walls = [ { p1 = vec2 176 16, p2 = vec2 432 16, normal = vec2 0 1}, { p1 = vec2 32 48, p2 = vec2 160 48, normal = vec2 0 1}, { p1 = vec2 448 48, p2 = vec2 608 48, normal = vec2 0 1}, { p1 = vec2 144 96, p2 = vec2 272 96, normal = vec2 0 1}, { p1 = vec2 336 96, p2 = vec2 464 96, normal = vec2 0 1} ]
     }
